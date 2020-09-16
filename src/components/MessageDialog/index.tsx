970d981d4c9d511462b2acc840e2ca80b747ebad
Keiichiro Ono <keiono@gmail.com>
2020-09-16 13:02:34 -0700

    Generic message dialog added.

---
+++ src/components/MessageDialog/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState, FC } from 'react'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+export type MessageDialogProps = {
+  open: boolean
+  setOpen: Function
+  title: string
+  message: string
+}
+
+const MessageDialog: FC<MessageDialogProps> = ({ message, open, setOpen }) => {
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Dismiss
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ErrorDialog
