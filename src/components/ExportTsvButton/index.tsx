ee07e027b5367bf2acab32942719dd9a581910a8
D Otasek <dotasek.dev@gmail.com>
2020-09-21 18:54:41 -0400

    Add placeholder for ExportTsv

---
+++ src/components/ExportTsvButton/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import DownloadIcon from '@material-ui/icons/CloudDownload'
+
+const DUMMY = {
+  data: ['a'],
+  test: 'C',
+}
+
+
+
+const ExportCxButton = ({cx}) => {
+  let disabled = true
+  if(cx !== undefined) {
+    disabled = false
+  }
+
+  const exportCx = (content, fileName, contentType) => {
+    const a = document.createElement('a')
+    const file = new Blob([content], { type: contentType })
+    a.href = URL.createObjectURL(file)
+    a.download = fileName
+    a.click()
+  }
+
+  const handleClick = () => {
+    exportCx(JSON.stringify(cx), 'dummy-file-name.json', 'application/json')
+  }
+
+  return (
+    <IconButton disabled={disabled} onClick={handleClick}>
+      <DownloadIcon />
+    </IconButton>
+  )
+}
+
+export default ExportCxButton
