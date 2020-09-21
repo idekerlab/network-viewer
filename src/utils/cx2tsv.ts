1fc12ed040075c32a1ee718d100b727fa8e2404c
D Otasek <dotasek.dev@gmail.com>
2020-09-21 16:10:46 -0400

    Import missing functions, and clean up code

--- src/utils/cx2tsv.ts
+++ src/utils/cx2tsv.ts
@@ -1,4 +1,8 @@
-const removeTabsAndPipesFromString = (strToNormalize : String) => {
+import { CyNetworkUtils } from 'cytoscape-cx2js'
+
+const cyNetworkUtils = new CyNetworkUtils()
+
+const removeTabsAndPipesFromString = (strToNormalize: String) => {
     const strWithoutTabsOrPipes = strToNormalize.replace(/\t/g, ' ').replace(/\|/g, ' ');
     return strWithoutTabsOrPipes;
 };
@@ -20,41 +24,109 @@ const getNodeNameFromNodeObj = (nodeObj) => {
 };
 
 const getEdgeInfo = (niceCX, edgeId) => {
-    if ( !niceCX) return null;
+    if (!niceCX) return null;
 
     var edge = niceCX.edges[edgeId];
-    var edgeInfo = { 'id': edgeId,
-                    s: edge.s,
-                    t: edge.t,
-                    i: edge.i};
+    var edgeInfo = {
+        'id': edgeId,
+        s: edge.s,
+        t: edge.t,
+        i: edge.i
+    };
     var counter;
-    if ( niceCX.edgeAttributes && niceCX.edgeAttributes[edgeId]) {
+    if (niceCX.edgeAttributes && niceCX.edgeAttributes[edgeId]) {
         var edgeAttrs = niceCX.edgeAttributes[edgeId];
-        edgeAttrs.forEach ( (value, pname) => {
-            if ( pname != 'selected') {
-                if ( !edgeInfo[pname]) {
+        edgeAttrs.forEach((value, pname) => {
+            if (pname != 'selected') {
+                if (!edgeInfo[pname]) {
                     edgeInfo[pname] = value;
                 } else {
-                    while (edgeInfo[pname+ '_' + counter]) {
+                    while (edgeInfo[pname + '_' + counter]) {
                         counter++;
                     };
-                    edgeInfo[pname+ '_' + counter] = value;
+                    edgeInfo[pname + '_' + counter] = value;
                     counter++;
-                }   
+                }
             }
         });
     }
 
-    if ( niceCX.edgeCitations && niceCX.edgeCitations[edgeId]) {
+    if (niceCX.edgeCitations && niceCX.edgeCitations[edgeId]) {
         var citationList = [];
-        niceCX.edgeCitations[edgeId].forEach( ( citationId) => {
-            citationList.push ( niceCX.citations[citationId]);
+        niceCX.edgeCitations[edgeId].forEach((citationId) => {
+            citationList.push(niceCX.citations[citationId]);
         });
-        if (citationList.length >0 )
+        if (citationList.length > 0)
             edgeInfo['citations'] = citationList;
     }
 
     return edgeInfo;
+};
+
+const getNodeInfo = (niceCX, nodeId) => {
+    if (!niceCX) return null;
+
+    var node = niceCX.nodes[nodeId];
+
+    var nodeInfo = {
+        'id': nodeId,
+        '_cydefaultLabel': cyNetworkUtils.getDefaultNodeLabel(niceCX, node),
+        'n': node.n,
+        'r': node.r
+    };
+    var counter = 1;
+    if (niceCX.nodeAttributes && niceCX.nodeAttributes[nodeId]) {
+        var nodeAttrs = niceCX.nodeAttributes[nodeId];
+        nodeAttrs.forEach((value, pname) => {
+            if (pname != "selected") {
+                if (!nodeInfo[pname]) {
+                    nodeInfo[pname] = value;
+                } else {
+                    while (nodeInfo[pname + '_' + counter]) {
+                        counter++;
+                    };
+                    nodeInfo[pname + '_' + counter] = value;
+                    counter++;
+                }
+            }
+
+        });
+    }
+
+    return nodeInfo;
+};
+
+const getAttributeValue = (attribute) => {
+    var returnStr = '';
+
+    if (typeof (attribute) === 'undefined' || attribute === '' || attribute == null) {
+        return returnStr;
+    }
+    if (typeof (attribute) === 'string') {
+        return attribute;
+    }
+
+
+    if (typeof (attribute) === 'object') {
+
+        if (attribute.v) {
+
+            if (Array.isArray(attribute.v)) {
+
+                for (var i = 0; i < attribute.v.length; i++) {
+                    attribute.v[i] = removeTabsAndPipesFromString(attribute.v[i]);
+                }
+
+                returnStr = attribute.v.join('|');
+
+            } else if (typeof (attribute.v) === 'string') {
+
+                returnStr = attribute.v;
+            }
+        }
+    }
+
+    return returnStr;
 };
 
 export const getTSVOfCurrentNiceCX = (niceCX) => {
@@ -87,7 +159,7 @@ export const getTSVOfCurrentNiceCX = (niceCX) => {
 
     var edgeAttributes = network.edgeAttributes;
 
-    var headers = {'Source Node': 0, Interaction: 1, 'Target Node': 2, 'Source ID': 3, 'Source Alias': 4};
+    var headers = { 'Source Node': 0, Interaction: 1, 'Target Node': 2, 'Source ID': 3, 'Source Alias': 4 };
     var sourceAliasOrder = headers['Source Alias'];
     var headersSize = 0;
 
@@ -98,10 +170,9 @@ export const getTSVOfCurrentNiceCX = (niceCX) => {
     var nodeAttributes = network.nodeAttributes;
     var aliasColumnHeader = null;
 
-    for (var key in nodeKeys)
-    {
+    for (var key in nodeKeys) {
         var nodeId = nodeKeys[key];
-        
+
         if (nodeAttributes) {
 
             var nodeAttrs = nodeAttributes[nodeId];
@@ -133,14 +204,13 @@ export const getTSVOfCurrentNiceCX = (niceCX) => {
         }
     }
 
-    headers['Target ID']    = Object.keys(headers).length;
+    headers['Target ID'] = Object.keys(headers).length;
     headers['Target Alias'] = Object.keys(headers).length;
-    var targetAliasOrder    = headers['Target Alias'];
+    var targetAliasOrder = headers['Target Alias'];
 
 
 
-    for (var key in nodeKeys)
-    {
+    for (var key in nodeKeys) {
         var nodeId = nodeKeys[key];
 
         if (nodeAttributes) {
@@ -169,10 +239,9 @@ export const getTSVOfCurrentNiceCX = (niceCX) => {
     }
 
     headers['Citation'] = Object.keys(headers).length;
-    var citationOrder   = headers['Citation'];
+    var citationOrder = headers['Citation'];
 
-    for(let i = 0; i < edgeKeys.length; i++ )
-    {
+    for (let i = 0; i < edgeKeys.length; i++) {
         var edgeKey = edgeKeys[i];
 
         if (edgeAttributes) {
@@ -194,49 +263,48 @@ export const getTSVOfCurrentNiceCX = (niceCX) => {
         }
     }
 
-    headers['cx edge id']        = Object.keys(headers).length;
+    headers['cx edge id'] = Object.keys(headers).length;
     headers['cx source node id'] = Object.keys(headers).length;
     headers['cx target node id'] = Object.keys(headers).length;
 
 
     var headersKeysSorted = Object.keys(headers);
-    headersKeysSorted.sort(function(a, b) {
+    headersKeysSorted.sort(function (a, b) {
         return headers[a] - headers[b];
     });
     var headerKeysJoined = headersKeysSorted.join('\t') + '\n';
 
-    var fileString      = headerKeysJoined;
+    var fileString = headerKeysJoined;
 
     var row = {};
 
-    var nodeAttributesAlreadyProcessed = {'n':0,  'r':0, 'id':0, '_cydefaultLabel':0};
-    var edgeAttributesAlreadyProcessed = {'id':0, 's':0, 't':0,  'i':0};
+    var nodeAttributesAlreadyProcessed = { 'n': 0, 'r': 0, 'id': 0, '_cydefaultLabel': 0 };
+    var edgeAttributesAlreadyProcessed = { 'id': 0, 's': 0, 't': 0, 'i': 0 };
 
     // Generate Source Node, Source Node Represents, Target Node, Target Node Represents,
     // if present
-    for(let i = 0; i < edgeKeys.length; i++ )
-    {
+    for (let i = 0; i < edgeKeys.length; i++) {
         for (key in headers) {
             row[key] = '';
         };
 
         var rowStringTemp = '';
-        var edgeKey       = edgeKeys[i];
-        var edgeObj       =  getEdgeInfo(network, edgeKey);
+        var edgeKey = edgeKeys[i];
+        var edgeObj = getEdgeInfo(network, edgeKey);
 
 
-        row['cx edge id']        = (edgeObj && edgeObj.id) ? edgeObj.id : '';
-        row['cx source node id'] = (edgeObj && (edgeObj.s || edgeObj.s == 0)) ? edgeObj.s  : '';
-        row['Interaction']       = (edgeObj && edgeObj.i)  ? edgeObj.i  : '';
-        row['cx target node id'] = (edgeObj && (edgeObj.t || edgeObj.t == 0)) ? edgeObj.t  : '';
+        row['cx edge id'] = (edgeObj && edgeObj.id) ? edgeObj.id : '';
+        row['cx source node id'] = (edgeObj && (edgeObj.s || edgeObj.s == 0)) ? edgeObj.s : '';
+        row['Interaction'] = (edgeObj && edgeObj.i) ? edgeObj.i : '';
+        row['cx target node id'] = (edgeObj && (edgeObj.t || edgeObj.t == 0)) ? edgeObj.t : '';
 
 
-        var sourceNodeObj = factory.getNodeInfo(edgeObj['s']);
-        var targetNodeObj = factory.getNodeInfo(edgeObj['t']);
+        var sourceNodeObj = getNodeInfo(network, edgeObj['s']);
+        var targetNodeObj = getNodeInfo(network, edgeObj['t']);
 
-        row['Source Node']    = getNodeNameFromNodeObj(sourceNodeObj);
+        row['Source Node'] = getNodeNameFromNodeObj(sourceNodeObj);
         row['Source ID'] = (sourceNodeObj && sourceNodeObj.r) ? sourceNodeObj.r : '';
-        row['Target Node']    = getNodeNameFromNodeObj(targetNodeObj);
+        row['Target Node'] = getNodeNameFromNodeObj(targetNodeObj);
         row['Target ID'] = (targetNodeObj && targetNodeObj.r) ? targetNodeObj.r : '';
 
         // get Source Node attributes
