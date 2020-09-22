160ccf1c36d4b6825ef73edc2a2eb633f9137078
Keiichiro Ono <keiono@gmail.com>
2020-09-21 23:47:57 -0700

    Top page updated to use the URL in the config file

--- src/components/TopPanel/index.tsx
+++ src/components/TopPanel/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import logo from '../../assets/images/ndex-logo.svg'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,22 +15,48 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     message: {
-      padding: '2em'
-    }
+      padding: '1em',
+    },
+    ndexLogo: {
+      height: '20vh',
+      padding: '1em',
+      '&:hover': {
+        cursor: 'pointer',
+        color: theme.palette.secondary.main,
+      },
+    },
+    link: {
+      '&:hover': {
+        fontWeight: 700,
+        cursor: 'pointer',
+        color: theme.palette.secondary.main,
+      },
+    },
   }),
 )
 
-const TopPage = (props) => {
+const TopPage = ({ config }) => {
   const classes = useStyles()
+
+  const url = config.ndexHttps
+
+  const handleClick = () => {
+    window.open(url, '_self')
+  }
 
   return (
     <div className={classes.root}>
-      <div className={classes.item} >
-        <Typography className={classes.message} variant="h2">
-          Please specify UUID of the network in the URL
+      <div className={classes.item}>
+        <img alt="NDEx Logo" src={logo} className={classes.ndexLogo} onClick={handleClick} />
+        <Typography className={classes.message} variant="h4">
+          Please specify UUID of the network
+        </Typography>
+
+        <Typography className={classes.link} onClick={handleClick} variant="body1">
+          Back to NDEx Home
         </Typography>
       </div>
     </div>
