111f1c9e9aa4de397766f2ab5f30f4aa9280d33e
Sophie Liu <sol015@ucsd.edu>
2020-09-27 16:57:22 -0400

    Formatted @context attribute so that broken links do not appear

--- src/components/DataPanel/NetworkPropertyPanel/NetworkProperties.jsx
+++ src/components/DataPanel/NetworkPropertyPanel/NetworkProperties.jsx
@@ -6,6 +6,10 @@ import ListItemText from '@material-ui/core/ListItemText'
 import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 
+import Collapse from '@material-ui/core/Collapse'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+
 import Linkify from 'linkifyjs/react'
 import parse from 'html-react-parser'
 
@@ -49,10 +53,22 @@ const NetworkProperties = (props) => {
   let index = 0
 
   const propertiesList = [['Description', formatContent(description)]]
+  let hasContext = false
+  let context = ''
   for (let datum of data) {
-    if (datum.value.trim() !== '') {
-      propertiesList.push([datum.predicateString, formatContent(datum.value.trim())])
+    const value = datum.value.trim()
+    const predicate = datum.predicateString.trim()
+    if (value !== '') {
+      if (predicate === '@context') {
+        hasContext = true
+        context = value
+      } else {
+        propertiesList.push([predicate, formatContent(value)])
+      }
     }
+  }
+  if (hasContext) {
+    propertiesList.push(['@context', formatContext(context)])
   }
 
   const display = []
@@ -90,6 +106,61 @@ const formatContent = (string) => {
   string = string.toString().replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script\ *>/gi, '')
   string = parse(string)
   return <Linkify key={Math.random().toString()}>{string}</Linkify>
+}
+
+const formatContext = (string) => {
+  const returnArray = []
+  const context = JSON.parse(string)
+  for (const key in context) {
+    returnArray.push(
+      <tr key={Math.random().toString()}>
+        <td valign="top">
+          <Typography variant="body2">{key}</Typography>
+        </td>
+        <td valign="top" style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}>
+          <Typography variant="body2">{context[key]}</Typography>
+        </td>
+      </tr>,
+    )
+  }
+
+  const details = (
+    <table>
+      <tbody>{returnArray}</tbody>
+    </table>
+  )
+  const summary = (
+    <Typography component="span" variant="body2">
+      Click to view the namespaces associated with this network
+    </Typography>
+  )
+
+  return <ExpandPanel summary={summary} details={details} defaultExpanded={false} />
+}
+
+function ExpandPanel(props) {
+  const [open, setOpen] = React.useState(props.defaultExpanded)
+  let style
+
+  function handleClick() {
+    setOpen(!open)
+  }
+
+  return (
+    <React.Fragment>
+      <ListItem button onClick={handleClick} key={Math.random()} style={{ padding: 0 }}>
+        <ListItemText primary={props.summary} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem style={{ paddingTop: 0, paddingBottom: 0 }}>
+            <ListItemText primary={props.details} />
+          </ListItem>
+        </List>
+      </Collapse>
+    </React.Fragment>
+  )
 }
 
 export default NetworkProperties
