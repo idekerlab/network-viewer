cb413f1322f4beb3681307a6c148b434c20bea3c
Sophie Liu <sol015@ucsd.edu>
2020-09-29 13:28:22 -0400

    Changed the way ownership is verified to use getSignedInUser for all cases

--- src/components/FooterPanel/EditMetadataButton.tsx
+++ src/components/FooterPanel/EditMetadataButton.tsx
@@ -1,40 +1,36 @@
 import React, { FC, useContext, useEffect, useState } from 'react'
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import EditIcon from '@material-ui/icons/Edit'
 import AppContext from '../../context/AppState'
 import Tooltip from '@material-ui/core/Tooltip'
 import { getNdexClient } from '../../utils/credentialUtil'
 
-const useStyles = makeStyles((theme: Theme) => createStyles({}))
-
 const EditMetadataButton: FC = () => {
-  const classes = useStyles()
   const { summary, ndexCredential } = useContext(AppContext)
   const [showButton, setShowButton] = useState(false)
 
   useEffect(() => {
     if (summary && ndexCredential.isLogin) {
-      if (ndexCredential.isGoogle) {
-        const ndexClient = getNdexClient('http://dev.ndexbio.org/v2', ndexCredential)
-        ndexClient.getSignedInUser().then((result) => {
-          if (result['emailAddress'] === ndexCredential.oauth['loginDetails'].profileObj.email) {
-            console.log('google')
+      const ndexClient = getNdexClient('http://dev.ndexbio.org/v2', ndexCredential)
+      ndexClient.getSignedInUser().then((result) => {
+        if (ndexCredential.isGoogle) {
+          if (result.emailAddress === ndexCredential.oauth['loginDetails'].profileObj.email) {
             if (!showButton) {
               setShowButton(true)
             }
           } else if (showButton) {
             setShowButton(false)
           }
-        })
-      } else if (ndexCredential.basic.userId === summary.owner) {
-        console.log('ndex')
-        if (!showButton) {
-          setShowButton(true)
+        } else {
+          if (result.properties.userName === ndexCredential.oauth['loginDetails'].profileObj.email) {
+            if (!showButton) {
+              setShowButton(true)
+            }
+          } else if (showButton) {
+            setShowButton(false)
+          }
         }
-      } else if (showButton) {
-        setShowButton(false)
-      }
+      })
     } else if (showButton) {
       setShowButton(false)
     }
