4c5222c9d33a9f2a3edadefae111058671f10d81
Keiichiro Ono <keiono@gmail.com>
2021-05-05 18:00:16 -0700

    UD-1697 Share buttons are disabled if not logged in

--- src/components/CreateDOIMenuItem/index.tsx
+++ src/components/CreateDOIMenuItem/index.tsx
@@ -1,22 +1,32 @@
-import React, {useEffect} from 'react'
+import React, { FC } from 'react'
 import MenuItem from '@material-ui/core/MenuItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import Typography from '@material-ui/core/Typography'
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
 
 import { getCurrentServer } from '../../utils/locationUtil'
 
 //http://dev.ndexbio.org/#/properties/network/9a17d903-85b9-11eb-8e98-525400c25d22/null?doi=true
 
-const CreateDOIMenuItem = ({uuid}) => {
-  return (
-    <MenuItem  component="a" href={getCurrentServer() + '/#/properties/network/' + uuid + '/null?doi=true&returnto=nnv'} >
+const CreateDOIMenuItem: FC<{ uuid: string; disabled: boolean }> = ({
+  uuid = '',
+  disabled = false,
+}): React.ReactElement => (
+  <MenuItem
+    disabled={disabled}
+    component="a"
+    href={
+      getCurrentServer() +
+      '/#/properties/network/' +
+      uuid +
+      '/null?doi=true&returnto=nnv'
+    }
+  >
     <ListItemIcon>
       <LibraryBooksIcon fontSize="small" />
     </ListItemIcon>
-    <Typography variant="inherit">Create DOI</Typography>
+    <Typography variant="inherit">Request DOI</Typography>
   </MenuItem>
-  )
-}
+)
 
 export default CreateDOIMenuItem
